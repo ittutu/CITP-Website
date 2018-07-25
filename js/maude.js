d492dffaaeba3a185f8d5f5871ca51c73138b8b6
Ionut Tutu <ittutu@gmail.com>
2018-07-25 18:28:16 +0100

    Corrections to source-code highlighting

--- js/maude.js
+++ js/maude.js
@@ -1,3 +1,7 @@
+// Disable the auto detection
+hljs.configure({languages: []});
+
+// Register Maude as a language
 hljs.registerLanguage("maude", function(hljs) {
   return {
     keywords: 'fmod endfm'
